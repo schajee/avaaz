31234504249f002c3d41f4101cf526f37292ab47
Shaji Ahmed <schajee@gmail.com>
2016-07-10 23:07:45 +0500

    Added Google Verification

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="google-site-verification" content="S-rm4Pi_Xed4Z61tPU8bA6WMDClSMNzhNs_k41oliWU" />
         @if (isset($description))
         <meta name="description" content="{{$description}}">
         @endif
